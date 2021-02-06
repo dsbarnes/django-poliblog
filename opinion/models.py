c2447cdb74fa31b8f36967f8248272afd59244e1
Derek Barnes <derekb0147@gmail.com>
2021-02-06 10:49:34 -0500

    cleaning up

--- opinion/models.py
+++ opinion/models.py
@@ -3,21 +3,21 @@ from django.utils.text import slugify
 from django.contrib.postgres.fields import ArrayField
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    email = models.CharField(max_length=255, blank=True)
-    site = models.CharField(max_length=255, blank=True)
+    first_name  =   models.CharField(max_length=25)
+    last_name   =   models.CharField(max_length=25)
+    email       =   models.CharField(max_length=255, blank=True)
+    site        =   models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
-    date = models.DateField()
-    slug = models.SlugField(blank=False, null=False)
+    title   =   models.CharField(max_length=255)
+    body    =   models.TextField()
+    author  =   models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
+    date    =   models.DateField()
+    slug    =   models.SlugField(blank=False, null=False)
 
     def __str__(self):
         return self.title
@@ -31,18 +31,18 @@ class Article(models.Model):
 
 
 class Video(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    vid = models.FileField()
-    name = models.CharField(max_length=25)
+    article     =   models.ForeignKey(Article, on_delete=models.CASCADE)
+    vid         =   models.FileField()
+    name        =   models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
 
 
 class Image(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    img = models.ImageField()
-    name = models.CharField(max_length=25)
+    article     =   models.ForeignKey(Article, on_delete=models.CASCADE)
+    img         =   models.ImageField()
+    name        =   models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
@@ -50,9 +50,9 @@ class Image(models.Model):
 
 # This is one, probably poor idea
 # class ArticleSubHeading(models.Model):
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-#     text = models.CharField(max_length=25)
-#     name = models.CharField(max_length=25)
+#     article   =   models.ForeignKey(Article, on_delete=models.CASCADE)
+#     text      =   models.CharField(max_length=25)
+#     name      =   models.CharField(max_length=25)
 
 #     def __str__(self):
 #         return self.name
