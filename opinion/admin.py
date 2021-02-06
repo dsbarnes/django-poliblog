a0f6f3a5efd0ef2c3fbe357a3b9c82e4944b443e
Derek Barnes <derekb0147@gmail.com>
2021-02-06 07:42:24 -0500

    autofill slug field with title in admin page

--- opinion/admin.py
+++ opinion/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Author, Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['img', 'title', 'body', 'author', 'date', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
+
 
 admin.site.register(Author)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
--- opinion/models.py
+++ opinion/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Author(models.Model):
@@ -17,7 +18,17 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateField()
     slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        # Do I want to have the datetime set?
+        # Or should I be able to pick, so that I can backdate?
+        # leaving commented out for now.
+        if not self.id:
+            self.slug = slugify(self.title)
+            # self.date = datetime.now()
+        super(Article, self).save(*args, **kwargs)
